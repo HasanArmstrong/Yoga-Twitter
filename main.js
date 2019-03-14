64dfc1dea89be28e7e707558bbb8c4492a7ac458
Manh Hung <etudeofmemories8698@gmail.com>
2019-03-14 17:51:09 +0700

    something something

---
+++ main.js
@@ -0,0 +1,13 @@
+/* ↓↓↓↓↓↓↓↓↓↓ MAIN-SECTION ↓↓↓↓↓↓↓↓↓↓ */
+
+let expandInput = () => {
+  let inputArea = document.querySelector('#tweet-input-wrapping');
+  inputArea.style.height = '80px';
+  let tweetBox = document.querySelector('#tweet-box');
+
+  document.querySelector('#button-row').style.display = 'block';
+}
+
+
+/* ↑↑↑↑↑↑↑↑↑↑ MAIN-SECTION ↑↑↑↑↑↑↑↑↑↑ */
+
